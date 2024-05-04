a421b76dc6faff87b64ee3dc48893a4c820af932
sriram <sriramjayashankar@gmail.com>
2024-05-04 17:55:04 +0530

    checks with the files in the faaces folder if name exists , while loop keeps running

--- Add_faces/main.py
+++ Add_faces/main.py
@@ -5,7 +5,12 @@
 
 import cv2 
 import time
+import os
 
+cwd = os.getcwd()
+
+# Get all the directory names in the current working directory
+os.chdir("Faces")
 # import cv2.qt
 
 # import cv2.qt.plugins
@@ -15,8 +20,17 @@ import time
 # cv2.qt.set_platformpluginpath("/usr/lib/x86_64-linux-gnu/qt6/plugins/platforms")
 
 
-def search_for_file_name():
-    return 
+def name_in_dir(name):
+
+    directories= os.listdir()
+
+
+#    Print all the directory names
+    for directory in directories:
+        if(name==directory):
+            return True
+    return False
+     
 def add_image_to_file():
     return
 
@@ -31,19 +45,27 @@ if not cap.isOpened():
     exit()
 
 #if exists
-name=input("enter the name")
+
+while True:
+    name=input("enter the name")
+    if(not name_in_dir(name)):
+        break
+    
 count=int(input("enter the number of images that have to be taken enter >0"))
 
 while count>0:
     ret,frame=cap.read()
-    #the image is captures ret=a boolean value if true image is captured if false image hasnt been captured properly
+    #the image is captures ret which is a boolean value if true image is captured if false image hasnt been captured properly
     #the frame is  anumpy asrray fo all the pixel values  
     time.sleep(1)
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    #conver the image to bgr format
+    #conver the image to bgr format to be read by the imshow function
     image=cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
     cv2.imshow('image',frame)
+    cv2.waitKey(1000)
     count=count-1
 
+cap.release()
+cv2.destroyAllWindows()
