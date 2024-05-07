eac483487a66f9d4b4387e7e10a69a64d59e89e2
sriram <sriramjayashankar@gmail.com>
2024-05-07 21:31:02 +0530

    added my face and getting started with capturing faces from video

--- Add_faces/file.py
+++ Add_faces/file.py
@@ -1,15 +1,6 @@
-'create a file if it doesnt exist , if it exists ask the user to provide a longer nane or somehting unique to identify the user'
+"""
+capture image from video using frame,
+take the frame from the video and then detect face with box coordinates ,
+add a box in the frame with those coordinates on the frame 
 
-import os
-
-# Get the current working directory
-cwd = os.getcwd()
-
-# Get all the directory names in the current working directory
-os.chdir("Faces")
-directories= os.listdir()
-print(type(directories[0]))
-
-# Print all the directory names
-for directory in directories:
-    print(directory)
+"""
